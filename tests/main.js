4abccfdb5d6fe49687ba53b4036d461beeadeddd
photonkhan <khan@interconnectionbd.com>
2019-06-11 13:28:48 +0600

    Added Fetch and Axios implementation

--- tests/main.js
+++ tests/main.js
@@ -1,10 +1,18 @@
 (function() {
 
     const postsApi = "https://heroku-fake-rest-api.herokuapp.com/posts"
+
     fetch(postsApi)
         .then((response) => response.json())
         .then(function(getData) {
             console.log(getData);
     })
 
+    axios.get(postsApi, {
+        params: {
+            _sort: "asc",
+            _page: "2",
+            _limit: "20"
+        }
+    }).then((response) => { console.log(response); })
 })();
