25cfdd91e63b182b5ff3ed03448d49e1e37eb37e
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-06-13 00:22:02 +0600

    Added new json server config

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
 
+// const port = process.env.PORT || 3000;
+
+// const command = `json-server --watch db.json --port ${port}`;
+
+// exec(command, (err, stdout, stderr) => {
+//     if (err) {
+//         console.log('Error running exec', err);
+//         return;
+//     }
+//     console.log('stdout:', stdout);
+//     console.log('stderr:', stderr);
+// });
+
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-const command = `json-server --watch db.json --port ${port}`;
+server.use(middlewares);
+server.use(router);
 
-exec(command, (err, stdout, stderr) => {
-    if (err) {
-        console.log('Error running exec', err);
-        return;
-    }
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-});
+server.listen(port);
